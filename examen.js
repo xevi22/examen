8d79f150693dbfb2bfb60b2fffd24df83bff540c
raul <raul.soneira@cirvianum.cat>
2021-11-30 16:14:47 +0100

    falten 45 min

--- examen.js
+++ examen.js
@@ -1,9 +1,23 @@
 window.onload = function () {
     // Variables
     let buttPrincipal = document.querySelector('#mainButt');
+    let usersArray = [];
+    //var nom = window.alert("Escriu el teu nom: ")
     //...
     
     // Funciones
-    
+    function addusers(){
+        usersArray.push("xevi");
+    }
+
     // Events
+    buttPrincipal.addEventListener(onclick,addusers());
+    var nom = window.prompt("Introdueix el teu nom: ");
+    var edat = window.prompt("Introdueix la teva edat: ");
+
+    var user = new Object();
+    
+
+
+    console.log(usersArray.length)
 } 
